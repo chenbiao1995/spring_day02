9c6be659e2c8d5a57eb1aff2f9763145d84cd60c
lenovo <lenovo@DESKTOP-IQCPEHJ>
2018-01-29 11:29:56 +0800

    spring的bean管理（注解）

---
+++ src/cn/itcast/anno/User.java
@@ -0,0 +1,14 @@
+package cn.itcast.anno;
+
+/**
+ * title:
+ * Description:
+ * @author: 
+ * @date:2018年1月29日
+ * @time:上午11:16:52
+ */
+public class User {
+	public void add() {
+		System.out.println("add....");
+	}
+}
